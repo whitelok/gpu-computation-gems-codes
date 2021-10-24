7b186101a9c3290426eb2133420bededc2f08efd
karlluo <karlluo@tencent.com>
2021-10-24 15:06:40 +0800

    dev test 2021年10月24日 星期日 15时06分40秒 CST

--- large_scale_gpu_search/large_scale_gpu_search.cu
+++ large_scale_gpu_search/large_scale_gpu_search.cu
@@ -1,6 +1,12 @@
+// Copyright 2021 karlluo. All rights reserved.
+//
+// Author: karlluo
+#include <thrust.h>
 
-// #include<thrust.h>
+template <typename T>
+void InitInputs(thrust::host_vector<T> &h_inputs_data,
+                thrust::host_vector<T> &h_keys,
+                thrust::device_vector<T> &d_inputs_data,
+                thrust::device_vector<T> &d_keys) {}
 
-int main(int argc, char* argv[]) {
-
-}
+int main(int argc, char *argv[]) {}
