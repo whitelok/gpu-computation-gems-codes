32921d10d6b8481b00d1089881b80bc7aee8d755
karlluo <karlluo@tencent.com>
2021-10-24 15:09:13 +0800

    dev test 2021年10月24日 星期日 15时09分13秒 CST

--- large_scale_gpu_search/large_scale_gpu_search.cu
+++ large_scale_gpu_search/large_scale_gpu_search.cu
@@ -13,4 +13,10 @@ void InitInputs(thrust::host_vector<T> &h_inputs_data,
                 thrust::device_vector<T> &d_inputs_data,
                 thrust::device_vector<T> &d_keys) {}
 
-int main(int argc, char *argv[]) {}
+int main(int argc, char *argv[]) {
+  thrust::host_vector<uint64_t> h_inputs_data;
+  thrust::host_vector<uint64_t> h_keys;
+  thrust::device_vector<uint64_t> d_inputs_data;
+  thrust::device_vector<uint64_t> d_keys;
+  InitInputs<uint64_t>(h_inputs_data, h_keys, d_inputs_data, d_keys);
+}
