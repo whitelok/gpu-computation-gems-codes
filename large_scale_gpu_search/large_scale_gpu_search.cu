e45ab9b700d4e1e4263994a6b8994bbed4cad38f
karlluo <karlluo@tencent.com>
2021-10-24 14:33:05 +0800

    dev test 2021年10月24日 星期日 14时33分05秒 CST

--- large_scale_gpu_search/large_scale_gpu_search.cu
+++ large_scale_gpu_search/large_scale_gpu_search.cu
@@ -1,4 +1,6 @@
-file(GLOB_RECURSE LARGE_SCALE_GPU_SEARCH_SRCS ${PROJECT_SOURCE_DIR}/large_scale_gpu_search/*.cc
-  ${PROJECT_SOURCE_DIR}/large_scale_gpu_search/*.cu)
 
-add_executable(large_scale_gpu_search LARGE_SCALE_GPU_SEARCH_SRCS)
+#include<thrust.h>
+
+int main() {
+
+}
