a98f323ebde22c358467aeccdd08ff3cd3b40995
karlluo <karlluo@tencent.com>
2021-10-24 16:03:07 +0800

    dev test 2021年10月24日 星期日 16时03分07秒 CST

--- large_scale_gpu_search/large_scale_gpu_search.cu
+++ large_scale_gpu_search/large_scale_gpu_search.cu
@@ -27,7 +27,7 @@ void InitInputs(const size_t data_numbers, const size_t keys_numbers,
                 thrust::device_vector<T> &d_keys) {
   curandGenerator_t curand_gen_handler;
   uint64_t * d_r;
-  cudaMalloc(&d_r, num * sizeof(uint64_t));
+  cudaMalloc(&d_r, data_numbers * sizeof(uint64_t));
   // Generating random uint64_t for search
   COMMON_CURAND_CHECK(curandCreateGenerator(&curand_gen_handler, CURAND_RNG_QUASI_SCRAMBLED_SOBOL64));
   COMMON_CURAND_CHECK(curandSetPseudoRandomGeneratorSeed(curand_gen_handler, 1278459ull));
